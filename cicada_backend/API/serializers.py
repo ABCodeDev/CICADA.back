a8f3ece6ffe7f9b5c67b24596d4003e9f1e98a99
Wisnu Teguh Wicaksono <cakson@live.it>
2017-02-27 19:03:39 +0700

    clean up codes on serializers

--- cicada_backend/API/serializers.py
+++ cicada_backend/API/serializers.py
@@ -4,6 +4,7 @@ import cicada_backend.models
 
 models = cicada_backend.models
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
@@ -16,6 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
             'username'
         ]
 
+
 class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
@@ -23,75 +25,90 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = models.Profile
         fields = '__all__'
 
+
 class Organization(serializers.ModelSerializer):
     class Meta:
         model = models.Organization
         fields = '__all__'
+
 
 class AdministratorAccess(serializers.ModelSerializer):
     class Meta:
         model = models.AdministratorAccess
         fields = '__all__'
 
+
 class Component(serializers.ModelSerializer):
     class Meta:
         model = models.Component
         fields = '__all__'
+
 
 class Notification(serializers.ModelSerializer):
     class Meta:
         model = models.Notification
         fields = '__all__'
 
+
 class TextField(serializers.ModelSerializer):
     class Meta:
         model = models.TextField
         fields = '__all__'
+
 
 class Timer(serializers.ModelSerializer):
     class Meta:
         model = models.Timer
         fields = '__all__'
 
+
 class Bill(serializers.ModelSerializer):
     class Meta:
         model = models.Bill
         fields = '__all__'
+
 
 class Form(serializers.ModelSerializer):
     class Meta:
         model = models.Form
         fields = '__all__'
 
+
 class Question(serializers.ModelSerializer):
     class Meta:
         model = models.Question
         fields = '__all__'
+
 
 class PossibleAnswer(serializers.ModelSerializer):
     class Meta:
         model = models.PossibleAnswer
         fields = '__all__'
 
+
 class UserNotificationFeed(serializers.ModelSerializer):
     class Meta:
         model = models.UserNotificationFeed
         fields = '__all__'
+
 
 class PaymentResponse(serializers.ModelSerializer):
     class Meta:
         model = models.PaymentResponse
         fields = '__all__'
 
+
 class FormResponse(serializers.ModelSerializer):
     class Meta:
         model = models.FormResponse
         fields = '__all__'
 
+
 class Answer(serializers.ModelSerializer):
     class Meta:
         model = models.Answer
         fields = '__all__'
+
 
 class UserComponentNotificationResponse(serializers.ModelSerializer):
     class Meta:
